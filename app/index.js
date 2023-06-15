78f0f029a7988558956fb3f202ab6b437e5ba59e
yosef langer <yosef@doar.systems>
2023-06-15 17:10:38 +0900

    first commit

---
+++ app/index.js
@@ -0,0 +1,116 @@
+const config = require('config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+const token = config.get('token');  // Replace with your actual static token
+const db = new sqlite3.Database(config.get('db').path, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the SQlite database.');
+});
+db.run('CREATE TABLE IF NOT EXISTS bot_control(bot_name text PRIMARY KEY, title text, description text, status INTEGER)', (err) => {
+    if (err) {
+        return console.log(err.message);
+    }
+    console.log('Table created.');
+});
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    let authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).send('Authorization header missing');
+        return;
+    }
+
+    let authParts = authHeader.split(' ');
+    if (authParts.length != 2 || authParts[0].toLowerCase() != 'bearer') {
+        res.status(401).send('Invalid authorization format');
+        return;
+    }
+
+    let providedToken = authParts[1];
+    if (providedToken != token) {
+        res.status(401).send('Invalid token');
+        return;
+    }
+
+    next();
+});
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/bots', (req, res) => {
+    let sql = 'SELECT * FROM bot_control';
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.json(rows.map(row => ({
+            botName: row.bot_name,
+            title: row.title,
+            description: row.description,
+            status: row.status === 1
+        })));
+    });
+});
+
+
+app.post('/bot/:bot_name', (req, res) => {
+    let data = [req.params.bot_name, req.body.title, req.body.description, req.body.status ? 1 : 0];
+    let checkSql = 'SELECT * FROM bot_control WHERE bot_name = ?';
+    db.get(checkSql, [req.params.bot_name], (err, row) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+
+        if (row) {
+            console.log(`Found bot: ${JSON.stringify(row)}`);
+            let updateSql = 'UPDATE bot_control SET status = ? WHERE bot_name = ?';
+            db.run(updateSql, [req.body.status ? 1 : 0, req.params.bot_name], (err) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).send(err);
+                }
+
+                res.json({status: req.body.status ? true : false});
+            });
+        } else {
+            console.log(`Inserting new bot: ${JSON.stringify(data)}`);
+            let insertSql = 'INSERT INTO bot_control(bot_name, title, description, status) VALUES(?, ?, ?, ?)';
+            db.run(insertSql, data, (err) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).send(err);
+                }
+
+                res.json({status: req.body.status ? true : false});
+            });
+        }
+    });
+});
+
+
+app.delete('/bot/:bot_name', (req, res) => {
+    let sql = 'DELETE FROM bot_control WHERE bot_name = ?';
+    db.run(sql, [req.params.bot_name], (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+
+        res.json({status: 'success'});
+    });
+});
+
+app.listen(config.get('port'), config.get('hostname'), () => {
+    console.log('Server running on port 3000');
+});
---
+++ public/js/index.js
@@ -0,0 +1,109 @@
+// Function to create a bot widget
+function createBotWidget(botName, title, description, status) {
+    var html = `
+        <div class="col-4 bot-widget" id="${botName}-widget">
+            <div class="card">
+                <div class="card-body">
+                    <button type="button" class="close delete-bot-widget-button d-none admin-mode" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${description}</p>
+                    <input type="checkbox" id="${botName}-switch" data-toggle="toggle" data-on="ON" data-off="OFF">
+                </div>
+            </div>
+        </div>
+    `;
+    $('#bot-control-panel').append(html);
+    $(`#${botName}-switch`).bootstrapToggle(status ? 'on' : 'off');
+    $(`#${botName}-switch`).change(function () {
+        let status = this.checked;
+        $.ajax({
+            url: `http://localhost:3000/bot/${botName}`,
+            type: 'POST',
+            data: JSON.stringify({status: status}),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            async: false
+        });
+    });
+    $(`#${botName}-widget .delete-bot-widget-button`).click(function () {
+        $.ajax({
+            url: `http://localhost:3000/bot/${botName}`,
+            type: 'DELETE',
+            success: function () {
+                $(`#${botName}-widget`).remove();
+            }
+        });
+    });
+}
+
+function showErrorModal(message) {
+    $("#errorModalMessage").text(message);
+    $("#errorModal").modal("show");
+}
+
+function applyAdminMode() {
+    jQuery('.admin-mode').removeClass('d-none');
+}
+
+$(document).ready(function () {
+    let urlParams = new URLSearchParams(window.location.search);
+    let adminMode = urlParams.get('admin_mode');
+    let token = urlParams.get('token');
+    // if admin mode is not set
+    if (adminMode === null || adminMode === undefined) {
+        urlParams.set('admin_mode', false); // set adminMode to the url
+        // set adminMode to the url
+        location.href = location.href.split('?')[0] + '?' + urlParams.toString();
+    }
+
+    const isAdminModeSet = adminMode === 'true';
+
+    // Configure AJAX setup
+    $.ajaxSetup({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            if (!$("#errorModal").hasClass('show')) {
+                showErrorModal("An error occurred during the request. Please provide a valid token.");
+            }
+        }
+    });
+
+    // Get the initial bot states
+    $.getJSON(`http://localhost:3000/bots`, (data) => {
+        data.forEach(bot => {
+            createBotWidget(bot.botName, bot.title, bot.description, bot.status);
+            if (isAdminModeSet) {
+                applyAdminMode();
+            }
+        });
+    });
+
+    // Handle the creation of new bot widgets
+    $('#create-widget-button').click(function () {
+        let botName = $('#bot-name-input').val();
+        let title = $('#bot-title-input').val();
+        let description = $('#bot-description-input').val();
+        $.ajax({
+            url: `http://localhost:3000/bot/${botName}`,
+            type: 'POST',
+            data: JSON.stringify({
+                title: title,
+                description: description
+            }),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function () {
+                createBotWidget(botName, title, description, false);
+                $('#create-widget-modal').modal('hide');
+                $('#create-widget-form')[0].reset();
+                if (isAdminModeSet) {
+                    applyAdminMode();
+                }
+            }
+        });
+    });
+});
